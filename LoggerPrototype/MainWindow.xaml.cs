9aac71c70b6fc967e64697d7c80f5373bcac203b
takase <taka.shun.2018@gmail.com>
2019-09-08 12:23:19 +0900

    プロジェクト ファイルを追加します。

---
+++ LoggerPrototype/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+
+namespace LoggerPrototype
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //LogTextScroll.ScrollToEnd();
+
+            var selectSerial = new SelectSerialPort();
+            selectSerial.OpenSerialPort = OpenSerialPortTest;
+            selectSerial.Show();
+        }
+
+        public void OpenSerialPortTest(string port, int baud)
+        {
+            SerialPort serialPort = new SerialPort();
+            serialPort.PortName = port;
+            serialPort.BaudRate = baud;
+            serialPort.NewLine = Environment.NewLine;
+            //SerialPort.Parity = System.IO.Ports.Parity.None;
+            //SerialPort.DataBits = 8;
+            //SerialPort.StopBits = System.IO.Ports.StopBits.One;
+            //SerialPort.Handshake = System.IO.Ports.Handshake.None;
+            serialPort.Open();
+
+            serialPort.DiscardInBuffer();
+            SerialLogTextBox.Document.Blocks.Clear();
+            PrintInfo("Started communication with " + serialPort.PortName);
+
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceivedHandler);
+
+        }
+
+        /// <summary>
+        /// 文字の出力：白
+        /// </summary>
+        /// <param name="str"></param>
+        public void Print(string str)
+        {
+            TextRange rangeOfText = new TextRange(SerialLogTextBox.Document.ContentEnd, SerialLogTextBox.Document.ContentEnd);
+            rangeOfText.Text = str;
+            rangeOfText.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.White);
+            SerialLogTextScroll.ScrollToEnd();
+        }
+
+        /// <summary>
+        /// 文字の出力：青
+        /// 最後に改行を入れる
+        /// </summary>
+        /// <param name="str"></param>
+        public void PrintInfo(string str)
+        {
+            TextRange rangeOfText = new TextRange(SerialLogTextBox.Document.ContentEnd, SerialLogTextBox.Document.ContentEnd);
+            rangeOfText.Text = "[INFO] " + str;
+            rangeOfText.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Blue);
+            SerialLogTextScroll.ScrollToEnd();
+            SerialLogTextBox.AppendText(Environment.NewLine);
+        }
+
+        /// <summary>
+        /// 文字の出力：赤
+        /// 最後に改行を入れる
+        /// </summary>
+        /// <param name="str"></param>
+        public void PrintWarning(string str)
+        {
+            TextRange rangeOfText = new TextRange(SerialLogTextBox.Document.ContentEnd, SerialLogTextBox.Document.ContentEnd);
+            rangeOfText.Text = "[WARN] " + str;
+            rangeOfText.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Red);
+            SerialLogTextScroll.ScrollToEnd();
+            SerialLogTextBox.AppendText(Environment.NewLine);
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //SerialLogTextBox.Text += "test\ntest\ntest\ntest\ntest\n";
+            //SerialLogTextBox.AppendText("test");
+
+            PrintWarning("test ");
+        }
+
+        private void SerialPort_DataReceivedHandler(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                SerialLogTextBox.Dispatcher.Invoke(
+                    new Action(() =>
+                    {
+                        SerialPort serialPort = (SerialPort)sender;
+                        string str = serialPort.ReadExisting();
+                        Print(str);
+                    })
+                );
+            }
+            catch
+            {
+                SerialLogTextBox.Dispatcher.Invoke(
+                    new Action(() =>
+                    {
+                        SerialPort serialPort = (SerialPort)sender;
+                        PrintWarning("Failed communication with " + serialPort.PortName);
+                    })
+                );
+            }
+        }
+
+        private void MenuTextItemClear_Click(object sender, RoutedEventArgs e)
+        {
+            SerialLogTextBox.Document.Blocks.Clear();
+        }
+
+        private void MenuFinish_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ LoggerPrototype/SelectSerialPort.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoggerPrototype
+{
+    /// <summary>
+    /// SelectSerialPort.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SelectSerialPort : Window
+    {
+        public Action<string, int> OpenSerialPort;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public SelectSerialPort()
+        {
+            InitializeComponent();
+
+            SetSerialPortName();
+            SetBaudRate();
+            Topmost = true;
+        }
+
+        /// <summary>
+        /// 現在接続されているSerialPort名を取得し，プルダウンメニューに表示
+        /// </summary>
+        public void SetSerialPortName()
+        {
+            var CheckComNum = new System.Text.RegularExpressions.Regex("COM[1-9][0-9]?[0-9]?");
+
+            System.Management.ManagementClass mcPnPEntity = new System.Management.ManagementClass("Win32_PnPEntity");
+            System.Management.ManagementObjectCollection manageObjCol = mcPnPEntity.GetInstances();
+
+            foreach (System.Management.ManagementObject manageObj in manageObjCol)
+            {
+                var namePropertyValue = manageObj.GetPropertyValue("Name");
+                if (namePropertyValue == null)
+                {
+                    continue;
+                }
+                string name = namePropertyValue.ToString();
+
+                if (CheckComNum.IsMatch(name))
+                {
+                    SerialComPort.Items.Add(name);
+                }
+            }
+            SerialComPort.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// 選択可能なボーレートをプルダウンメニューに表示
+        /// 必要な場合はここに追加
+        /// </summary>
+        public void SetBaudRate()
+        {
+            int[] baudRate = { 4800, 9600, 115200 };
+            foreach(var i in baudRate)
+            {
+                SerialBaudRate.Items.Add(i.ToString());
+            }
+            SerialBaudRate.SelectedIndex = 1;
+        }
+
+        /// <summary>
+        /// 選択したSerialPort名を取得
+        /// </summary>
+        /// <returns></returns>
+        public string GetSelectSerialPortName()
+        {
+            var ExtractPortNum = new System.Text.RegularExpressions.Regex(".*(COM[1-9][0-9]?[0-9]?).*");
+            if (SerialComPort.SelectedItem == null)
+            {
+                //textBoxTextArea.Text += "No Port Selected\n";
+                return System.String.Empty;
+            }
+            string name = (string)SerialComPort.SelectedItem;
+            return ExtractPortNum.Replace(name, "$1");
+        }
+
+
+        /// <summary>
+        /// 選択したBaudRateを取得
+        /// </summary>
+        /// <returns></returns>
+        public int GetSelectBaudRate()
+        {
+            string baudRateValue = (string)SerialBaudRate.SelectedItem;
+            return int.Parse(baudRateValue);
+        }
+
+        private void SerialStartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenSerialPort(GetSelectSerialPortName(), GetSelectBaudRate());
+            Close();
+        }
+
+        /// <summary>
+        /// SelectSerialPort Windowを閉じる
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SerialCancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// SerialPortの名前を再取得
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SerialUpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SerialComPort.Items.Clear();
+            SetSerialPortName();
+        }
+    }
+}
