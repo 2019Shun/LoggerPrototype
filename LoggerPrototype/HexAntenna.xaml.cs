4057074b41816d26e78eead6442b89f48c765cbe
takase <taka.shun.2018@gmail.com>
2019-09-11 14:37:13 +0900

    [Add] 6面体アンテナ選択ウィンドウ

---
+++ LoggerPrototype/HexAntenna.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoggerPrototype
+{
+    /// <summary>
+    /// HexAntenna.xaml の相互作用ロジック
+    /// </summary>
+    public partial class HexAntenna : Window
+    {
+        public Action<string> SerialWriteString;
+
+        public HexAntenna()
+        {
+            InitializeComponent();
+        }
+
+        private void AV1_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AV1");
+        }
+
+        private void AH1_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AH1");
+        }
+
+        private void AV2_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AV2");
+        }
+
+        private void AH2_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AH2");
+        }
+
+        private void AV3_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AV3");
+        }
+
+        private void AH3_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AH3");
+        }
+
+        private void AV4_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AV4");
+        }
+
+        private void AH4_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AH4");
+        }
+
+        private void AV5_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AV5");
+        }
+
+        private void AH5_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AH5");
+        }
+
+        private void AV6_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AV6");
+        }
+
+        private void AH6_Click(object sender, RoutedEventArgs e)
+        {
+            SerialWriteString("*AH6");
+        }
+    }
+}
